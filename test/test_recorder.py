3d59cbb91999e02bea40352b822c33beb39e68b8
Andrew Fowlie <andrew.j.fowlie@qq.com>
2022-11-23 12:44:47 +0800

    add test for recorder

---
+++ test/test_recorder.py
@@ -0,0 +1,30 @@
+"""
+Unit testing of recorder
+========================
+"""
+
+
+import unittest
+
+from kingpin import recorder
+
+
+class TestRecorder(unittest.TestCase):
+
+    def test_record(self):
+        r1 = recorder.Record(1, 10)
+        r2 = recorder.Record(4, 10)
+        r = r1 + r2
+        self.assertEqual(r.efficiency, 0.2)
+
+    def test_recorder(self):
+        r1 = recorder.Recorder()
+        r2 = recorder.Recorder()
+        r1["e"] = recorder.Record(1, 10)
+        r2["e"] = recorder.Record(4, 10)
+        r2["f"] = recorder.Record(7, 10)
+        r = r1 + r2
+        self.assertEqual(str(r), 'e = 0.200. f = 0.412')
+
+if __name__ == '__main__':
+    unittest.main()
